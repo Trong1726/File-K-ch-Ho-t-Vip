1494f7607ce2face96f9b19f11750f05838d1e2b
Trong1726 <123875384+Trong1726@users.noreply.github.com>
2025-02-08 15:51:59 +0700

    Moule-locketgold.js

--- Moule-locketgold.js
+++ Moule-locketgold.js
@@ -2,7 +2,7 @@
 #!desc=By:  Phạm Trọng-GenzPVPT.Com
 
 [Script]
-revenuecat = type=http-response, pattern=^https:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/[^/]+$), script-path=https://raw.githubusercontent.com/quocchienn/lockcrack/refs/heads/module/Locket_Gold.js, requires-body=true, max-size=-1, timeout=60
+revenuecat = type=http-response, pattern=^https:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/[^/]+$), script-path=https://raw.githubusercontent.com/Trong1726/File-K-ch-Ho-t-Vip/refs/heads/main/Locketgold1.js, requires-body=true, max-size=-1, timeout=60
 
 deleteHeader = type=http-request, pattern=^https:\/\/api\.revenuecat\.com\/.+\/(receipts|subscribers), script-path=https://raw.githubusercontent.com/Trong1726/File-K-ch-Ho-t-Vip/refs/heads/main/Locketgold.js, timeout=60
 
